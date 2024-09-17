0a8c98382672a8765469d00e8ada55493932b6fa
christoph3r3w <144007933+christoph3r3w@users.noreply.github.com>
2024-09-17 10:29:23 +0200

    added main content
    - added main sections
    - imported external libraries
    - imported API data as person

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,19 @@
 <script>
+	/** @type {import('./$types').PageData} */
+	// import gsap from "gsap";
+	// import LocomotiveScroll from 'locomotive-scroll';
+	// import '../styles/main.css';
+	export let data;
+	let people = data.people
+	export let peep = people? people.find(person => person.id == 40) : 'not found';
+
+	console.log(peep);
 	
-	import gsap from "gsap";
-	import LocomotiveScroll from 'locomotive-scroll';
+
+	// console.log('lol',Peep);
+	
+	
+	
 
 	// const locomotiveScroll = new LocomotiveScroll();
 </script>
@@ -12,19 +24,101 @@
 </svelte:head>
 
 <main>
-	<ul>
-		<li id="cover-1"><a href="#cover-2">1</a></li>
-		<li id="cover-2"><a href="#cover-3">2</a></li>
-		<li id="cover-3"><a href="#cover-4">3</a></li>
-		<li id="cover-4"><a href="#cover-5">4</a></li>
-		<li id="cover-5"><a href="#cover-1">5</a></li>
+	<section id="cover-1" class="covers" tabindex="0">
+		<a href="#cover-2" class="frame">
+			<div class="pics-container">
+				<!-- <span class="profile profile-1">
+					<img  src="{peep.avatar}" alt="">
+				</span> -->
+				<span class="profile profile-2 flower" >
+					<img src="/25acb22a-22a3-41d5-a0eb-c91529c4c6c8.jpg" width=300 alt="image of {peep.name}">
+					<!-- <img src="https://portfolio-lti-production.s3.eu-west-1.amazonaws.com/cvq44juv5nrywjfbnlgdrwaweabo?response-content-disposition=inline%3B%20filename%3D%226992026a-4ec4-4399-a36a-c18411945cb2.JPG%22%3B%20filename%2A%3DUTF-8%27%276992026a-4ec4-4399-a36a-c18411945cb2.JPG&response-content-type=image%2Fjpeg&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAZUHNSBLFH5JMYGQV%2F20240915%2Feu-west-1%2Fs3%2Faws4_request&X-Amz-Date=20240915T210806Z&X-Amz-Expires=43200&X-Amz-SignedHeaders=host&X-Amz-Signature=131003c97f68d35b7ea57099e9da85018ae1decef2bad55b4a8a12c7fc12e219" width=300 alt="image of {peep.name}"> -->
+				</span>
+
+			</div>
+			
+			<h1 class="name">{peep.name} {peep.surname}</h1>
+		</a>
+	</section>
+	<section id="cover-2" class="covers " tabindex="0">
+		<a href="#cover-3" class="frame">
+			<div class="side-info scalloped-box">
+				<ul>
+					<li> squad-id {peep.squad_id}</li>
+					<li> role {peep.role}</li>
+					<li> hobbies {peep.bio}</li>
+					<li> and more </li>
+				</ul>
+			</div>
+			<div class="bio">
+				<p>	bio {peep.bio}</p>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae rem vero nobis perferendis, quas id! Debitis nostrum, delectus culpa voluptates veritatis voluptatibus earum ratione dignissimos id magni, inventore molestiae ducimus.
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae rem vero nobis perferendis, quas id! Debitis nostrum, delectus culpa voluptates veritatis voluptatibus earum ratione dignissimos id magni, inventore molestiae ducimus.
+				</p>
+			</div>
+			<div class="side-pic">
+				<img src="/IMG_8452.JPEG" alt="{peep.name}">
+			</div>
+			<!-- <div class="about-shape scalloped-box">
+
+			</div> -->
+		</a>
+	</section>
+	<section id="cover-3" class="covers frame" tabindex="0">
 		
+			<section class="mainG">
+				<article class="top top_1">
+					<iframe src="https://book-of-potions.onrender.com/brew" frameborder="0" tabindex="-1"></iframe>
+				</article>  
+				<article class="top top_2">2</article>
+				<article class="top top_3">3</article>
+					
+				<article class="mid mid_1">4</article>  
+				<article class="mid mid_2">
+					<a href="#cover-5" class="frame stamp box" style="color: transparent;">
+						next
+					</a>
+
+					
+				</article>
+				<article class="mid mid_3">6</article>
+					
+				<article class="down down_1">
+					<a href="https://github.com/christoph3r3w">
+						<img src="/GitHub_Logo.png" alt="github logo" width="300">
+					</a>
+				</article>  
+				<article class="down down_2">
+					<iframe height="300" style="width: 100%;" scrolling="no" title="Untitled" src="https://codepen.io/Christopher-the-animator/embed/NWmxrpY?default-tab=" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true" tabindex="-1">
+						See the Pen <a href="https://codepen.io/Christopher-the-animator/pen/NWmxrpY">
+						Untitled</a> by Christopher (<a href="https://codepen.io/Christopher-the-animator">@Christopher-the-animator</a>)
+						
+					  </iframe>				
+				</article>
+				<article class="down down_3">
+					<iframe src="https://brilletjes-squad-page.onrender.com" tabindex="-1"></iframe>
+				</article>
+			</section> 
 		
-		
-		
-		
-	</ul>
+	</section>
+	<!-- <section id="cover-4" class="covers" tabindex="0">
+		<a href="#cover-5" class="frame">4</a>
+	</section> -->
+	<section id="cover-5" class="covers" tabindex="0">
+		<a href="#cover-1" class="frame">
+			
+			<ul>
+				<li>insta</li>
+				<li>github</li>
+				<li>linkedin</li>
+				<li>arena</li>
+			</ul>
+		</a>
+	</section>
 </main>
+
+
 
 <style>
 	
